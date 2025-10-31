87a5c334adbf8a54bf05402b751f2f7b01161c35
Andy <andy.su7117@gmail.com>
2025-10-31 15:29:31 +1100

    Initialised TCP Server

---
+++ WebServer.py
@@ -0,0 +1,28 @@
+#coding: utf-8
+from socket import *
+import sys
+
+if (len(sys.argv) != 2):
+    print("Incorrect arguments")
+    sys.exit
+
+serverPort = 12000 
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+serverSocket.bind(('localhost', serverPort))
+
+serverSocket.listen(1)
+
+print("The server is ready to receive")
+
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+
+    sentence = connectionSocket.recv(1024)
+
+    capitalizedSentence = sentence.upper()
+
+    connectionSocket.send(capitalizedSentence)
+
+    connectionSocket.close()
